67819fc9acce1b50dda4bc21726a3787ec349c57
cobra-kai-03 <78267022+cobra-kai-03@users.noreply.github.com>
2023-01-27 19:53:03 +0530

    Create Token bucket .cpp

---
+++ cnlab/Token bucket .cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+class TokenBucket {
+    public:
+        TokenBucket(int tokens, int time_unit, void (*forward_callback)(int), void (*drop_callback)(int)) {
+            this->tokens = tokens;
+            this->time_unit = time_unit;
+            this->forward_callback = forward_callback;
+            this->drop_callback = drop_callback;
+            this->bucket = tokens;
+            this->last_check = std::chrono::system_clock::now();
+        }
+
+        void handle(int packet) {
+            auto current = std::chrono::system_clock::now();
+            auto time_passed = std::chrono::duration_cast<std::chrono::milliseconds>(current - this->last_check).count();
+            this->last_check = current;
+
+            this->bucket = this->bucket + (time_passed * (this->tokens / this->time_unit));
+
+            if (this->bucket > this->tokens) {
+                this->bucket = this->tokens;
+            }
+
+            if (this->bucket < 1) {
+                this->drop_callback(packet);
+            } else {
+                this->bucket = this->bucket - 1;
+                this->forward_callback(packet);
+            }
+        }
+
+    private:
+        int tokens;
+        int time_unit;
+        void (*forward_callback)(int);
+        void (*drop_callback)(int);
+        int bucket;
+        std::chrono::time_point<std::chrono::system_clock> last_check;
+};
+
+void forward(int packet) {
+    std::cout << "Packet Forwarded: " << packet << std::endl;
+}
+
+void drop(int packet) {
+    std::cout << "Packet Dropped: " << packet << std::endl;
+}
+
+int main() {
+    TokenBucket throttle(1, 1, forward, drop);
+
+    int packet = 0;
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        throttle.handle(packet);
+        packet += 1;
+    }
+}
