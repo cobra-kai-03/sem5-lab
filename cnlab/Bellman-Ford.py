58cee133883651ebd69a98f2bdd5ae76dad9639a
cobra-kai-03 <78267022+cobra-kai-03@users.noreply.github.com>
2023-01-27 21:35:08 +0530

    Update Bellman-Ford.py

--- cnlab/Bellman-Ford.py
+++ cnlab/Bellman-Ford.py
@@ -1,73 +1,42 @@
-# Python3 program for Bellman-Ford's single source
-# shortest path algorithm.
-
-# Class to represent a graph
-
 class Graph:
+    def __init__(self,n):
+        self.v=n
+        self.glist=[]
 
-	def __init__(self, vertices):
-		self.V = vertices # No. of vertices
-		self.graph = []
+    def addedge(self,u,v,d):
+        self.glist.append([u,v,d])
+    
+    def bellman(self,source):
+        dist=[float("Inf")]*self.v
+        dist[source]=0
 
-	# function to add an edge to graph
-	def addEdge(self, u, v, w):
-		self.graph.append([u, v, w])
+        for i in range(self.v-1):
+            for u,v,d in (self.glist):
+                if(dist[u]!=float("Inf") and dist[u]+d<dist[v]):
+                    dist[v]=dist[u]+d
 
-	# utility function used to print the solution
-	def printArr(self, dist):
-		print("Vertex Distance from Source")
-		for i in range(self.V):
-			print("{0}\t\t{1}".format(i, dist[i]))
-
-	# The main function that finds shortest distances from src to
-	# all other vertices using Bellman-Ford algorithm. The function
-	# also detects negative weight cycle
-	def BellmanFord(self, src):
-
-		# Step 1: Initialize distances from src to all other vertices
-		# as INFINITE
-		dist = [float("Inf")] * self.V
-		dist[src] = 0
-
-		# Step 2: Relax all edges |V| - 1 times. A simple shortest
-		# path from src to any other vertex can have at-most |V| - 1
-		# edges
-		for _ in range(self.V - 1):
-			# Update dist value and parent index of the adjacent vertices of
-			# the picked vertex. Consider only those vertices which are still in
-			# queue
-			for u, v, w in self.graph:
-				if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-					dist[v] = dist[u] + w
-
-		# Step 3: check for negative-weight cycles. The above step
-		# guarantees shortest distances if graph doesn't contain
-		# negative weight cycle. If we get a shorter path, then there
-		# is a cycle.
-
-		for u, v, w in self.graph:
-			if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-				print("Graph contains negative weight cycle")
-				return
-
-		# print all distance
-		self.printArr(dist)
+        for u,v,d in (self.glist):
+                if(dist[u]!=float("Inf") and dist[u]+d<dist[v]):
+                    print("Graph has negative cycles")
+                    return
+        print("Distances calculated")
+        for i in range(self.v):
+            print(i," : ",dist[i])
 
 
-# Driver's code
-if __name__ == '__main__':
-    n=int(input('Enter number of edges  '))
-    g = Graph(n)
-#i=0
-    while(n):
+n=int(input('Enter number of vertices  '))
+m=int(input('Enter number of edges  '))
+g = Graph(n)
+
+print("The vertices are : ")
+for i in range(n):
+    print(i)
+while(m>=1):
         a=int(input('Enter vertex1:  '))
         b=int(input('Enter vertex2:  '))
         c=int(input('Enter distance: '))
-        g.addEdge(a,b,c)
-        n-=1
+        g.addedge(a,b,c)
+        m-=1
 
-
-	
-
-	# function call
-    g.BellmanFord(0)
+src=int(input("Enter source vertex"))
+g.bellman(src)
